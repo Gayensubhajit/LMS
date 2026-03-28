289ade849e92750914aa0f26cf716b65061ed9e2
Subhajit Gayen <subhajitgayen43@gmail.com>
2026-03-28 23:11:05 +0530

    .

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,12 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-// Group matchers for clarity and scale
-const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sing-up(.*)"]);
-const isProtectedRoute = createRouteMatcher([
-  "/profile(.*)",
-  "/my-courses(.*)",
-  "/settings(.*)",
-  "/accomplishments(.*)",
-  "/support(.*)",
-  "/course/(.*)", // PROTECT SPECIFIC COURSE-IDs, Keeps /course public
-]);
-
-export default clerkMiddleware(async (auth, req) => {
-  // Protect specific routes
-  if (isProtectedRoute(req)) {
-    await auth.protect();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Skip Next internals and static files
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
-    "/(api|trpc)(.*)",
+    '/(api|trpc)(.*)',
   ],
 };
